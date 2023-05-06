c02e748acaf3b038854e1289e88fafec2c2b9c6b
AlexPixel <alexandregavriilidis@gmail.com>
2023-05-06 16:01:30 +0200

    Set up project and Add state

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,16 @@
 </template>
 
 <script>
-
+  import axios from 'axios';
   export default {
+    created() {
+      const url = "https://fakestoreapi.com/products";
+      
+      axios
+      .get(url)
+      .then((response)=>console.log(response.data))
+      .catch((error)=>console.error(error));
+    }
 
   }
   
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore } from 'vuex'
 
 export default createStore({
+  // It will the source of truth, where all the data 
+  // of application will be centralized
   state: {
-    
+    products: [1, 2, 3]
   },
   mutations: {
     
